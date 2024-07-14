90882841a56133076d5da5d3ed7ebb2f106ef96c
Sarah-Stodder <sjsarahboo@gmail.com>
2024-07-14 14:53:56 -0600

    Updates for travis

--- welcome.py
+++ welcome.py
@@ -18,3 +18,15 @@ print(birthday_generator(29))
 #adding in more code so we can see the commit error message!
 
 print("Hello I love to write out code its oh so fun!")
+
+def birthday_generator2(age):
+    new_age = age + 1
+    if new_age < 21:
+        return f"You just had a birthday, you are now {new_age}"
+    elif new_age > 21 and new_age < 50:
+        return f"Its your birthday, go and party like an animal! You only turn {new_age} once!"
+    else:
+        return f"We wont tell anyone its your {new_age} birthday, your secret is safe!"
+print(birthday_generator2(16))
+print(birthday_generator2(29))
+print(birthday_generator2(70))
