e9f0545a6e4690cc94cfeb3d2a1232aa7f24e497
Sarah-Stodder <sjsarahboo@gmail.com>
2024-07-14 14:26:11 -0600

    first commit

---
+++ welcome.py
@@ -0,0 +1,15 @@
+# Hey this some cool coding stuff i did on sunday
+
+
+students = ["James","Kareem","Brian", "Zabdiel", "Pamauk"]
+
+for student in students:
+    print(student + " is going to crush next week!")
+
+
+
+def birthday_generator(age):
+    new_age = age + 1
+    return f"You just had a birthday, you are now {new_age} years old"
+print(birthday_generator(16))
+print(birthday_generator(29))
